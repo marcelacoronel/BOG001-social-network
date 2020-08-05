124a0366fe0464a51244349d337cc3aacd65742f
lizguro <dianyguti@gmail.com>
2020-08-05 08:57:54 -0500

    view signup

--- src/views/singup.js
+++ src/views/singup.js
@@ -3,25 +3,35 @@
 export default () => {
 
     const view =`
-
-        <div class="formulario">
-            <h2>Crea tu cuenta</h2>
-            <form action="#">
-                <input type="text" placeholder="usuario" required>
-                <input type="password" placeholder="contraseña" required>
-                <input type="email" placeholder="correo electrónico" required>
-                <input type="text" placeholder="telefóno" required>
-                <input type="submit" onclick="Registrarse()" value="Registrarse">
-            </form>
+        
+        <div class="conteiner-form">
+            <div class=logo_singup>
+                <img img src="./img/logo_signup.svg" alt="logo_singup">
+            </div>
+            <div class="form-Singup">
+                <h2>REGISTRATE</h2>
+                <form action="#">
+                    <label for="Nombre">NOMBRE</label>
+                    <input id="Nombre" type="text" placeholder="pepito perez" required>
+                    <label for="">EMAIL</label>
+                    <input type="password" placeholder="pepitoperez@hotmail.com" required>
+                    <label for="">CONTRASEÑA</label>
+                    <input type="email" placeholder="***********" required>
+                    <input type="submit" onclick="Registrarse()" value="SING IN">
+                </form>
+            </div>
         </div>
-        <div class="resset">
-            <a href="">Olvide mi contraseña?</a>
+
+        <div class=bg-singup>
+            <img img src="./img/SingUp-desktop.svg" alt="bg_singup">
         </div>
 `;
 
    const divElement = document.createElement(`div`);
-   divElement.classList = 'contenedor'
+   divElement.classList = 'SingUp-Conteiner'
    divElement.innerHTML =view;
+
+   document.getElementById("headerNav").style.display="none";
 
     return divElement;
 }
