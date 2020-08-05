c29c83fb7715193340f55a8ec26f08a07b911d24
lizguro <dianyguti@gmail.com>
2020-08-05 17:32:37 -0500

    signup responsive

--- src/views/singup.js
+++ src/views/singup.js
@@ -4,13 +4,13 @@ export default () => {
 
     const view =`
         
-        <div class="conteiner-form">
+        <div class="container-form">
             <div class=logo_singup>
-                <img img src="./img/logo_signup.svg" alt="logo_singup">
+                <img src="./img/logo_signup.svg" alt="logo_singup">
             </div>
             <div class="form-Singup">
-                <h2>REGISTRATE</h2>
-                <form action="#">
+                <h2>Registrate</h2>
+                <form action="#" class="form-SU-Desktop-tablet">
                     <label for="Nombre">NOMBRE</label>
                     <input id="Nombre" type="text" placeholder="pepito perez" required>
                     <label for="">EMAIL</label>
@@ -18,17 +18,41 @@ export default () => {
                     <label for="">CONTRASEÑA</label>
                     <input type="email" placeholder="***********" required>
                     <input type="submit" onclick="Registrarse()" value="SING IN">
+                    <span>
+                    <p>Ya tienes cuenta? <a href="#/login">Log in</a> </p>
+                    </span>
+                </form>
+                <form action="#" class="form-SU-movil">
+                    <input id="Nombre" type="text" placeholder="Nombre" required>
+                    <input type="password" placeholder="Email" required>
+                    <input type="email" placeholder="Contraseña" required>
+                    <input type="submit" onclick="Registrarse()" value="SING IN">
+                    <span>
+                        <p>Ya tienes cuenta? <a href="#/login">Log in</a> </p>
+                    </span>
                 </form>
             </div>
         </div>
 
         <div class=bg-singup>
-            <img img src="./img/SingUp-desktop.svg" alt="bg_singup">
+            <img class="desktop-SU" img src="./img/SingUp-desktop.svg" alt="bg_singup">
+            <img class="tablet-SU" img src="./img/SingUp-tablet.png" alt="bg_singup">
+            <img class="mobile-SU" img src="./img/SingUp-mobile.png" alt="bg_singup">
+        </div>
+
+        <div id="landscape">
+            <div>
+	            <p>
+	                <img src="./img/giro@2x.png" alt=""><br><center>
+                    ¡Gira tu dispositivo!</center><br>
+                    <span><center>¡Please!</center></span>
+	            </p>
+            </div>
         </div>
 `;
 
    const divElement = document.createElement(`div`);
-   divElement.classList = 'SingUp-Conteiner'
+   divElement.classList = 'SingUp-Container'
    divElement.innerHTML =view;
 
    document.getElementById("headerNav").style.display="none";
