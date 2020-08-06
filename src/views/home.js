cee3ed277f436afd7e0f43ba0f21cdaf72977cf4
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-08-06 08:21:29 -0500

    maquetación basica

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,68 @@
 export default () => {
     const view = `
-        <h1> Hello World </h1>
-        <p> with js routers </p>
+      
+       <img id ="logo-home" src="/src/img/logo-BluePink.png" alt="logo">
+       
+       <div class="homeContainer-desktop">
+        <img id ="main-img" src="/src/img/doggie.gif" alt="dog-friend">
+        </div>
+
+        <div class="homeContainer-mobile">   
+        <button class="appStartbtn btn"><a href="#/login">Ingresar</a></button>
+         <img id="mobileMain-img" src= "/src/img/Home-mobile.svg">
+         </div>
+
+      
     `;
 
-   const divElement = document.createElement(`div`);
-   divElement.innerHTML =view;
+    // function clickFun() {
+    //     window.location = '#/login';
+    // }
+
+    const nav = document.getElementById("header-nav")
+    nav.style.display = "block";
+
+    const divElement = document.createElement(`div`);
+    divElement.innerHTML = view;
+
+
+
 
     return divElement;
 }
+//  <div class="mobile" style="display:none;"></div>
+//         <button id="appStartbtn btn">Ingresar</button>
+//         <img id="foto3" src= "/src/img/Home-mobile.svg">
+//          </div>
+
+// export default () => {
+// const view = () => {
+
+//     `
+//    <div class="home">
+//     <img id ="foto2" src="/src/img/Home1-desktop.svg">
+//     </div>
+//     <div class="mobile"></div>
+//     <button id="startApp">Ingresar</button>
+//     <img id="foto3" src= "/src/img/Home-mobile.svg">
+//      </div>
+// `
+// const divElement = document.createElement(`div`);
+// divElement.innerHTML =view;
+//  return divElement;
+
+//     }
+//     const viewMobile = () => {
+
+//         `
+//         <div class="mobile"></div>
+//         <button id="startApp">Ingresar</button>
+//         <img id="foto3" src= "/src/img/Home-mobile.svg">
+//          </div>
+//     `
+//     const divElement = document.createElement(`div`);
+//     divElement.innerHTML =viewMobile;
+//      return divElement;
+
+//         }
+//     }
