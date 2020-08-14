f0b28f68a90877a2d4913244115db5338108dcb3
marcelacoronel <marcelacoronelp@gmail.com>
2020-08-14 10:25:43 -0500

    ajustando cambios para hacer test

---
+++ test/auth.spec.js
@@ -0,0 +1,8 @@
+import {signInUsers} from '../src/components/auth.js';
+
+describe('signInUsers', () => {
+    it.only('debería ser una función', () => {
+      expect(typeof myFunction).toBe('function');
+    });
+  });
+  
